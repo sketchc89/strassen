077ff89c3ea077c8f859908b190138108a6c83bf
Chris Sketch <sketchc89@gmail.com>
2021-04-08 01:44:56 -0400

    make transpose member function, initializer list ctr

--- include/strassen.h
+++ include/strassen.h
@@ -1,6 +1,8 @@
 #pragma once
 
 #include <array>
+#include <initializer_list>
+#include <ostream>
 #include <utility>
 
 namespace strassen {
@@ -11,6 +13,13 @@ class Mat {
 public:
     /*! @brief default constructor */
     Mat() = default;
+    /*! @brief initialize with 2d initializer list
+     *  @code
+     *  strassen::Mat<int32_t, 2, 2> mat{{1, 2}, {3, 4}};
+     *  @endcode
+     */
+    Mat(std::initializer_list<std::initializer_list<T>> l);
+
     // rule of 5
     /*! @brief copy constructor */
     Mat(const Mat& other) = default;
@@ -23,38 +32,46 @@ public:
     /* !@brief default destructor */
     virtual ~Mat() = default;
 
+    /* @brief friend matrix with different template parameters
+     * @details used to allow member functions to directly access _data rather than
+     *          use getters, for example when multiplying
+     */
     template <typename _T, int32_t R2, int32_t C2>
     friend class Mat;
 
+    /*! @brief matrix multiplication
+     *  @details compile time check for invalid multiplication.
+     *           only allows for same type multiplication since it's ambiguous how to cast
+     */
     template <typename _T = T, int32_t R1 = R, int32_t C1 = C, int32_t R2, int32_t C2>
     Mat<_T, R1, C2> operator*(const Mat<_T, R2, C2>& rhs);
-
+    /*! @brief values in matrix are exactly equal to one another */
     bool operator==(const Mat& rhs);
+    /*! @brief values in matrix are not exactly equal to one another */
     bool operator!=(const Mat& rhs);
-
+    /*! @brief gets the value stored at row and column */
     T get(int32_t row, int32_t col) const;
+    /*! @brief sets the row and column given to val */
     void set(int32_t row, int32_t col, T val);
+    /*! @brief returns number of rows and columns as a pair */
     std::pair<int32_t, int32_t> dim() const noexcept;
+
+    /*! @brief tranposes matrix
+     *  @details creates a new matrix where the rows and columns are flipped such that
+     *           a value previously at r, c will now be at c, r
+     */
+    Mat<T, C, R> transpose();
 
 
 private:
     std::array<std::array<T, C>, R> _data;
 };
 
-/*! @brief tranposes matrix
- *  @details creates a new matrix where the rows and columns are flipped such that
- *           a value previously at r, c will now be at c, r
- */
-template <typename T, int32_t R, int32_t C>
-static Mat<T, C, R> transpose(const Mat<T, R, C>& mat);
-
 }  // namespace strassen
 
-
-/*! @brief multiplication operator for matrices
- *  @details creates a new matrix with dimensions equal to the #rows of the first operand and the # of columns of the second operand */
-// template <typename T, int32_t R1, int32_t C1, int32_t R2, int32_t C2>
-// strassen::Mat<T, R1, C2> operator*(const strassen::Mat<T, R1, C1>& lhs, const strassen::Mat<T, R2, C2>& rhs);
+template <typename T, int32_t R, int32_t C>
+std::ostream& operator<<(std::ostream& os, const strassen::Mat<T, R, C>& mat);
 
 
+// implementation of class
 #include <strassen.hpp>
--- include/strassen.hpp
+++ include/strassen.hpp
@@ -1,13 +1,6 @@
+/*! implements strassen.h DO NOT include directly */
+
 #include <cassert>
-
-
-// template <typename T, int32_t R1, int32_t C1, int32_t R2, int32_t C2>
-// strassen::Mat<T, R1, C2> operator*(const strassen::Mat<T, R1, C1>& lhs, const strassen::Mat<T, R2, C2>& rhs) {
-//     static_assert(C1 == R2, "Matrix operands are invalid for multiplication.");
-//
-//     auto lhsRes = strassen::split(lhs);
-//     auto rhsRes = strassen::split(rhs);
-
 
 namespace strassen {
 
@@ -15,7 +8,6 @@ template <typename T, int32_t R, int32_t C>
 template <typename _T, int32_t R1, int32_t C1, int32_t R2, int32_t C2>
 Mat<_T, R1, C2> Mat<T, R, C>::operator*(const Mat<_T, R2, C2>& rhs) {
     static_assert(C1 == R2, "Matrix operands are invalid for multiplication.");
-
 
     Mat<T, R1, C2> res;
     for (int32_t i = 0; i < R1; ++i) {
@@ -37,11 +29,33 @@ bool Mat<T, R, C>::operator==(const Mat<T, R, C>& rhs) {
             }
         }
     }
+    return true;
 }
 
 template <typename T, int32_t R, int32_t C>
 bool Mat<T, R, C>::operator!=(const Mat<T, R, C>& rhs) {
     return !operator==(rhs);
+}
+
+template <typename T, int32_t R, int32_t C>
+std::ostream& operator<<(std::ostream& os, const strassen::Mat<T, R, C>& mat) {
+    for (int32_t row = 0; row < R; ++row) {
+        for (int32_t col = 0; col < C; ++col) {
+            os << mat.get(row, col) << ',';
+        }
+        os << '\n';
+    }
+    return os;
+}
+
+template <typename T, int32_t R, int32_t C>
+Mat<T, R, C>::Mat(std::initializer_list<std::initializer_list<T>> l) {
+    assert(l.size() == R && begin(l)->size() == C);
+    auto lIt = begin(l);
+    auto aIt = begin(_data);
+    for (; lIt != end(l) && aIt != end(_data); ++lIt, ++aIt) {
+        std::copy(lIt->begin(), lIt->end(), aIt->begin());
+    }
 }
 
 template <typename T, int32_t R, int32_t C>
@@ -62,11 +76,11 @@ std::pair<int32_t, int32_t> Mat<T, R, C>::dim() const noexcept {
 }
 
 template <typename T, int32_t R, int32_t C>
-Mat<T, C, R> transpose(const Mat<T, R, C>& mat) {
+Mat<T, C, R> Mat<T, R, C>::transpose() {
     Mat<T, C, R> res;
     for (int32_t row = 0; row < R; ++row) {
         for (int32_t col = 0; col < C; ++col) {
-            res.set(col, row, mat.get(row, col));
+            res._data[col][row] = _data[row][col];
         }
     }
     return res;
